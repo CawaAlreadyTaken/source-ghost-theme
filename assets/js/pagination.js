1d08d2b6b6e695832e27ac4daf6c5c23b168d180
CawaAlreadyTaken <daniele.cabassi01@gmail.com>
2025-12-03 23:47:24 +0100

    Debugging

--- assets/js/pagination.js
+++ assets/js/pagination.js
@@ -1,123 +1,70 @@
-//function pathnameParser(pathname) {
-//  if (!/page/.test(pathname)) {
-//    return pathname;
-//  }
-//
-//  return pathname.split("page")[0];
-//}
+function pathnameParser(pathname) {
+  if (!/page/.test(pathname)) {
+    return pathname;
+  }
 
-//function navButtonFormatter(nextOrPrevEl, nextOrPrevName, paginationEl) {
-//  const icon = nextOrPrevName === "previous" ? "left" : "right";
-//  const className = nextOrPrevName === "previous" ? "sm-prev" : "sm-next";
-//
-//  const navButton = nextOrPrevEl
-//    ? document.createElement("a")
-//    : document.createElement("span");
-//  navButton.innerHTML = `<svg aria-hidden="true"><use href="#sm-${icon}-arrow-icon"></use></svg>`;
-//  navButton.classList.add("sm-circle-icon-button", className);
-//
-//  if (nextOrPrevEl) {
-//    navButton.setAttribute("aria-label", `${nextOrPrevName} posts`);
-//    navButton.setAttribute("href", nextOrPrevEl);
-//  } else {
-//    navButton.classList.add("sm-circle-icon-button", "sm-nav-disabled");
-//  }
-//
-//  paginationEl.append(navButton);
-//}
-//
-//export default function generatePagination() {
-//  const pagination = document.querySelector(".sm-pagination");
-//  
-//  if (!pagination) return;
-//  pagination.setAttribute("aria-label", "page selector");
-//  const pathname = pathnameParser(window.location.pathname);
-//  const { page, prev, next, pages } = pagination.dataset;
-//
-//  navButtonFormatter(prev, "previous", pagination);
-//
-//  const paginationStart = page - 2 > 0 ? page - 2 : 1;
-//
-//  for (
-//    let index = paginationStart - 1;
-//    index < Math.min(paginationStart + 4, +pages);
-//    index += 1
-//  ) {
-//    let urlPath;
-//
-//    if (index === 0 && pathname === "/") {
-//      urlPath = "/";
-//    } else if (index === 0 && pathname !== "/") {
-//      urlPath = pathname;
-//    } else {
-//      urlPath = `${pathname}page/${index + 1}/`;
-//    }
-//
-//    const div = document.createElement("div");
-//    div.classList.add("sm-pagination-item");
-//    const a = document.createElement("a");
-//    a.setAttribute("href", urlPath);
-//    a.textContent = index + 1;
-//
-//    if (+page === index + 1) {
-//      div.classList.add("sm-current");
-//    }
-//    div.append(a);
-//    pagination.append(div);
-//  }
-//
-//  navButtonFormatter(next, "next", pagination);
-//}
+  return pathname.split("page")[0];
+}
 
+function navButtonFormatter(nextOrPrevEl, nextOrPrevName, paginationEl) {
+  const icon = nextOrPrevName === "previous" ? "left" : "right";
+  const className = nextOrPrevName === "previous" ? "sm-prev" : "sm-next";
 
+  const navButton = nextOrPrevEl
+    ? document.createElement("a")
+    : document.createElement("span");
+  navButton.innerHTML = `<svg aria-hidden="true"><use href="#sm-${icon}-arrow-icon"></use></svg>`;
+  navButton.classList.add("sm-circle-icon-button", className);
 
-function pagination(isInfinite = true, done, isMasonry = false) {
-    const feedElement = document.querySelector('.gh-feed');
-    if (!feedElement) return;
+  if (nextOrPrevEl) {
+    navButton.setAttribute("aria-label", `${nextOrPrevName} posts`);
+    navButton.setAttribute("href", nextOrPrevEl);
+  } else {
+    navButton.classList.add("sm-circle-icon-button", "sm-nav-disabled");
+  }
 
-    let loading = false;
-    const target = document.querySelector('.gh-footer');
-    const buttonElement = document.querySelector('.gh-loadmore');
+  paginationEl.append(navButton);
+}
 
-    if (!document.querySelector('link[rel=next]') && buttonElement) {
-        buttonElement.remove();
+export default function generatePagination() {
+  const pagination = document.querySelector(".sm-pagination");
+  
+  if (!pagination) return;
+  pagination.setAttribute("aria-label", "page selector");
+  const pathname = pathnameParser(window.location.pathname);
+  const { page, prev, next, pages } = pagination.dataset;
+
+  navButtonFormatter(prev, "previous", pagination);
+
+  const paginationStart = page - 2 > 0 ? page - 2 : 1;
+
+  for (
+    let index = paginationStart - 1;
+    index < Math.min(paginationStart + 4, +pages);
+    index += 1
+  ) {
+    let urlPath;
+
+    if (index === 0 && pathname === "/") {
+      urlPath = "/";
+    } else if (index === 0 && pathname !== "/") {
+      urlPath = pathname;
+    } else {
+      urlPath = `${pathname}page/${index + 1}/`;
     }
 
-    const loadNextPage = async function () {
-        const nextElement = document.querySelector('link[rel=next]');
-        if (!nextElement) return;
+    const div = document.createElement("div");
+    div.classList.add("sm-pagination-item");
+    const a = document.createElement("a");
+    a.setAttribute("href", urlPath);
+    a.textContent = index + 1;
 
-        try {
-            const res = await fetch(nextElement.href);
-            const html = await res.text();
-            const parser = new DOMParser();
-            const doc = parser.parseFromString(html, 'text/html');
+    if (+page === index + 1) {
+      div.classList.add("sm-current");
+    }
+    div.append(a);
+    pagination.append(div);
+  }
 
-            const postElements = doc.querySelectorAll('.gh-feed:not(.gh-featured):not(.gh-related) > *');
-            const fragment = document.createDocumentFragment();
-            const elems = [];
-
-            postElements.forEach(function (post) {
-                var clonedItem = document.importNode(post, true);
-
-                if (isMasonry) {
-                    clonedItem.style.visibility = 'hidden';
-                }
-
-                fragment.appendChild(clonedItem);
-                elems.push(clonedItem);
-            });
-
-            feedElement.appendChild(fragment);
-
-            if (done) {
-                done(elems, loadNextWithCheck);
-            }
-
-            const resNextElement = doc.querySelector('link[rel=next]');
-            if (resNextElement && resNextElement.href) {
-                nextElement.href = resNextElement.href;
-            } else {
-                nextElement.remove();
-                if (buttonElement) {
-                    buttonElement.remove();
+  navButtonFormatter(next, "next", pagination);
+}
